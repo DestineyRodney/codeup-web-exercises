ff242ba8087d5b6d0f1a95bbdf0eb738138ad8a4
Destiney Rodney <destiney.rodney01@gmail.com>
2021-08-12 15:42:42 -0500

    feat: Question 1 and 2 completed

--- js/External.js
+++ js/External.js
@@ -2,3 +2,8 @@
 console.log("Hello from external JavaScript.");
 alert("Welcome to my website!");
 
+var userInput = prompt("Please enter your favorite color.");
+    console.log("The user entered: " +  userInput);
+    alert("Great, " + userInput + " is my favorite color too!");
+
+
