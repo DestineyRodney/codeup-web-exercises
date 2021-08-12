62be03bc5ddf44e26c8602f944b8da22875db602
Destiney Rodney <destiney.rodney01@gmail.com>
2021-08-12 15:22:08 -0500

    feat: I created inline_js.html and said Hello from inline JavaScript. I also updated title to Inline Js.

--- js/External.js
+++ js/External.js
@@ -1,8 +1,2 @@
 "use strict";
-console.log("Hello from external JS");
-alert("Hey this is external file");
-var myStr = true;
-console.log(myStr);
-
-// var userConfirm = confirm("Do you want to remain logged in?")
-// alert(" The user selected: " + userConfirm);
+console.log("Hello from external JavaScript.");
