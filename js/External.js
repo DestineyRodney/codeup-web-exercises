99f9b8599db9c8a48220a2795164a0c0ad352c1b
Destiney Rodney <destiney.rodney01@gmail.com>
2021-08-12 14:41:27 -0500

    feat:Welcome External

---
+++ js/External.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log("Hello from external JS");
+alert("Hey this is external file");
+var myStr = true;
+console.log(myStr);
+
+// var userConfirm = confirm("Do you want to remain logged in?")
+// alert(" The user selected: " + userConfirm);
