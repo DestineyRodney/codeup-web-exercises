0e36869a552f6021d94ab0da3b0b4fbe097159ff
Destiney Rodney <destiney.rodney01@gmail.com>
2021-08-18 10:16:35 -0500

    feat: Completed for_loops exercises.

---
+++ js/for_loops.js
@@ -0,0 +1,27 @@
+// 2
+// function showMultiplicationTable (num) {
+//     for (var i = 1; i < 10; i++) {
+//         console.log(num + "x" + "=" + (num * i));
+//     }
+// }
+// showMultiplicationTable(7);
+
+//3
+
+// for (var i = 25; i <=200; i++){
+//     if (i % 2 == 0) console.log( i + " Our number is even");
+//     else console.log(i +" Our number is odd");
+// }
+
+
+//4
+for (a = 1; a <= 9; a++) {
+    for (b = 1; b <= a; b++) {
+        console.log(a);
+    }
+}
+
+// 5
+// for (var d = 100; d >=5; d = d - 5){
+//     console.log(d)
+// }
