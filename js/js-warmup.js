11f1549803a7897acb925eb19bb0e01cd717b85e
Destiney Rodney <destiney.rodney01@gmail.com>
2021-09-02 09:44:01 -0500

    feat: JS warm-up completed.

---
+++ js/js-warmup.js
@@ -0,0 +1,14 @@
+function tellFortune (numChildren, partnerName, location, jobTitle) {
+    return "You will be a " + jobTitle + " in " + location + ", and married to " + partnerName + " with " + numChildren + " kids."
+}
+
+console.log(tellFortune(3, "tK", "Italy", "Designer"));
+console.log(tellFortune(1, "Nat", "London", "Developer"));
+
+// console.log("Hello");
+
+// function tellFortune(numChildren, partnerName, location, jobTitle) {
+//     var output = "You will be have" + numChildren + "children.";
+//
+//     return output;
+// }
