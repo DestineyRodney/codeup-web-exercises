135dc8e51a72553957da73fa00401af23166ba19
Destiney Rodney <destiney.rodney01@gmail.com>
2021-09-28 14:15:07 -0500

    feat: Completed jquery exercise.

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,7 +1,29 @@
 "use strict";
 
-$(document).ready(function() {
+// // Exercise Pt.1
+// $(document).ready(function() {
+// //
+//     alert("Ready to Go!")
+//
+// });
 
-    alert("Ready to Go!")
+// var content = $('#main-header').html();
+// alert(content);
+//
+// $('#codeup').css('color', 'blue').css('font-size', '40px').css('text-align', 'center').html('Happy Coding');
+//
+// // Exercise Pt. 2
+//
+// $('.codeup').css('border', '1px solid red');
+//
 
-});
+// Exercise pt. 3
+
+$('li').css('font-size', '20px');
+$('h1, p, li').css('background-color', 'yellow');
+
+var content = $('h1').html();
+alert(content);
+
+
+
