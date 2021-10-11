e97009c6c61f82b957e62cc967f899c9dcb58240
Destiney Rodney <destiney.rodney01@gmail.com>
2021-10-11 15:26:35 -0500

    feat: displayed current weather with icon

--- js/weather-map-project.js
+++ js/weather-map-project.js
@@ -1,66 +1,32 @@
 "use strict";
 //
 
-// $.get('https://api.openweathermap.org/data/2.5/forecast', {
-//     appid: weatherKey,
-//     q: "San Antonio, US",
-//     lat: 29.7030,
-//     lon: -98.1245,
-//
-//     units: 'imperial'
-//
-// })
-//     .done(function(data) {
-//         console.log(data) ;
-//
-//     });
+$.get('https://api.openweathermap.org/data/2.5/weather', {
+    appid: weatherKey,
+    q: "San Antonio, US",
+    lat: 29.7030,
+    lon: -98.1245,
+
+    units: 'imperial'
+
+})
+    .done(function(data) {
+        console.log(data) ;
+        //
+        var iconcode = data.weather[0].icon;
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+
+$(".icon").attr('src', iconurl);
+$(".weather").html( "Current Weather: " + data.weather[0].description.toUpperCase());
+$(".temperature").html("Current Temperature: " + data.main.temp + " °F");
+$(".temp-high-low").html("Low: " + data.main.temp_min+ " °F" + "  High: " + data.main.temp_max+ " °F");
+$(".location").html(data.name);
+$(".wind-speeds").html("Current Wind Speed: " + data.wind.speed + " mph");
+$(".date").html(data.dt)
+    });
 
 
+function getWeather (forecast) {
 
-// $(document).ready(function () {
-//
-//     $("submitWeather").click(function () {
-//         var city = $("city").val();
-//
-//         if (city !== ""){
-//             $.ajax({
-//                 url: "api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&APPID=weatherKey",
-//                 type: "GET",
-//             dataType: "jsonp",
-//             success: function(data){
-//                     console.log(data);
-//             }
-//             });
-//         }else {
-//             $("error").html("Field cannot be empty");
-//         }
-//     });
+}
 
-// });
-
-$.get("https://api.openweathermap.org/data/2.5/weather", {
-    APPID: weatherKey,
-    q:     "San Antonio, US",
-    units: "imperial",
-}).done(function(data) {
-    console.log(data);
-    //insert code inside the .done
-    var temperatureF = data.temp;
-
-    $("#weather").html("<p>" + data.weather[0].main + "</p>");
-    $("#temperature").html(temperatureF + "&#176;" + "<span>F</span>");
-    $("#icon").html('<img src="img/weather-icons/' + data.weather[0].icon + '.png" + />');
-    $("#location").html(data.name);
-    $("#wind-speed").html("Current Wind Speed: " + data.wind.speed);
-
-});
-//
-//     // ==== Makes timestamp human readable ====
-//     function getDate(timestamp) {
-//         return new Date(timestamp * 1000).toLocaleDateString();
-//     }
-//
-//
-//     // === Function Call ===
-//
-//
