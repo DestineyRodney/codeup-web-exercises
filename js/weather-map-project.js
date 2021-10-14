82f73e40a076b05008d1e4f66c1c319d2dadcbac
Destiney Rodney <destiney.rodney01@gmail.com>
2021-10-14 09:48:18 -0500

    feat: updated project with some functionality to search.

--- js/weather-map-project.js
+++ js/weather-map-project.js
@@ -15,13 +15,13 @@ $.get('https://api.openweathermap.org/data/2.5/weather', {
         var iconcode = data.weather[0].icon;
         var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
 
-$(".icon").attr('src', iconurl);
-$(".weather").html( "Current Weather: " + data.weather[0].description.toUpperCase());
-$(".temperature").html("Current Temperature: " + data.main.temp + " °F");
-$(".temp-high-low").html("Low: " + data.main.temp_min+ " °F" + "  High: " + data.main.temp_max+ " °F");
-$(".location").html(data.name);
-$(".wind-speeds").html("Current Wind Speed: " + data.wind.speed + " mph");
-$(".date").html(data.dt)
+// $(".icon").attr('src', iconurl);
+// $(".weather").html( "Current Weather: " + data.weather[0].description.toUpperCase());
+// $(".temperature").html("Current Temperature: " + data.main.temp + " °F");
+// $(".temp-high-low").html("Low: " + data.main.temp_min+ " °F" + "  High: " + data.main.temp_max+ " °F");
+// $(".location").html(data.name);
+// $(".wind-speeds").html("Current Wind Speed: " + data.wind.speed + " mph");
+// $(".date").html(data.dt)
 
 
 
@@ -33,73 +33,78 @@ $(".date").html(data.dt)
         appid: weatherKey,
         lat: 29.7030,
         lon: -98.1245,
-        exclude: "current, minutely, hourly, alerts",
+        exclude: "current,minutely,hourly,alerts",
 
-        units: 'imperial',
+        units: "imperial",
 
     })
         .done(function (data) {
             console.log(data);
+            //refactor here with function
+            for(let i = 0; i <= 4; i++){
+                let iconcode = data.daily[i].weather[0].icon;
+                var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+                //day 1
+                $('.icon').eq(i).attr('src', iconurl);
+                $('.dt').eq(i).html("date: " + data.daily[i].dt);
+                $('.description').eq(i).html("Weather: " + data.daily[i].weather[0].description.toUpperCase());
+                $('.temp').eq(i).html("Day Temperature: " + data.daily[i].temp.day + " °F" );
+                $('.temphl').eq(i).html("Low: " + data.daily[i].temp.min + " °F" + " High: " + data.daily[i].temp.max + " °F");
 
-            let iconcode1 = data.daily[0].weather[0].description.icon;
-            var iconurl1 = "http://openweathermap.org/img/w/" + iconcode1 + ".png";
-            //day 1
-                $('.icon1').attr('src', iconurl1);
-                $('.dt1').html("date: " + data.daily[0].dt);
-                $('.description1').html("Weather: " + data.daily[0].weather[0].description.toUpperCase());
-                $('.temp1').html("Day Temperature: " + data.daily[0].temp.day + " °F" );
-                $('.temphl1').html("Low: " + data.daily[0].temp.min + " °F" + " High: " + data.daily[0].temp.max + " °F");
+                $('.wind-speed').eq(i).html(data.daily[i].wind_speed + " mph");
+            }
 
-            $('.wind-speed1').html(data.daily[0].wind_speed + " mph");
+            //eq goes through each item from list
 
-
-
-            //day2
-
-            $('.dt2').html("date: " + data.daily[1].dt);
-            $('.description2').html("Weather: " + data.daily[1].weather[0].description.toUpperCase());
-            $('.temp2').html("Day Temperature: " + data.daily[1].temp.day + " °F" );
-            $('.temphl2').html("Low: " + data.daily[1].temp.min + " °F" + " High: " + data.daily[0].temp.max + " °F");
-
-            $('.wind-speed2').html(data.daily[1].wind_speed + " mph");
-
-
-
-        //    day3
-
-            $('.dt3').html("date: " + data.daily[2].dt);
-            $('.description3').html("Weather: " + data.daily[2].weather[0].description.toUpperCase());
-            $('.temp3').html("Day Temperature: " + data.daily[2].temp.day + " °F" );
-            $('.temphl3').html("Low: " + data.daily[2].temp.min + " °F" + " High: " + data.daily[0].temp.max + " °F");
-
-            $('.wind-speed3').html(data.daily[2].wind_speed + " mph");
-
-            //day4
-
-            $('.dt4').html("date: " + data.daily[3].dt);
-            $('.description4').html("Weather: " + data.daily[3].weather[0].description.toUpperCase());
-            $('.temp4').html("Day Temperature: " + data.daily[3].temp.day + " °F" );
-            $('.temphl4').html("Low: " + data.daily[3].temp.min + " °F" + " High: " + data.daily[0].temp.max + " °F");
-
-            $('.wind-speed4').html(data.daily[3].wind_speed + " mph");
-
-
-        //    day5
-
-            $('.dt5').html("date: " + data.daily[4].dt);
-            $('.description5').html("Weather: " + data.daily[4].weather[0].description.toUpperCase());
-            $('.temp5').html("Day Temperature: " + data.daily[4].temp.day + " °F" );
-            $('.temphl5').html("Low: " + data.daily[4].temp.min + " °F" + " High: " + data.daily[0].temp.max + " °F");
-
-            $('.wind-speed5').html(data.daily[4].wind_speed + " mph");
 
         });
 
+$("#find").click(function(e){
+    e.preventDefault();
+    let city = $("#search").val();
+    geocode(city, mapboxKey).then((coor)=> {
+        console.log(coor);
+        $.get('https://api.openweathermap.org/data/2.5/onecall', {
+            appid: weatherKey,
+            lat: coor[1],
+            lon: coor[0],
+            exclude: "current,minutely,hourly,alerts",
+
+            units: "imperial",
+
+        }).done(function(data){
+            console.log(data);
+
+            for(let i = 0; i <= 4; i++){
+                let iconcode = data.daily[i].weather[0].icon;
+                var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+                //day 1
+                $('.icon').eq(i).attr('src', iconurl);
+                $('.dt').eq(i).html("date: " + data.daily[i].dt);
+                $('.description').eq(i).html("Weather: " + data.daily[i].weather[0].description.toUpperCase());
+                $('.temp').eq(i).html("Day Temperature: " + data.daily[i].temp.day + " °F" );
+                $('.temphl').eq(i).html("Low: " + data.daily[i].temp.min + " °F" + " High: " + data.daily[i].temp.max + " °F");
+
+                $('.wind-speed').eq(i).html(data.daily[i].wind_speed + " mph");
+            }
+        })
+
+    })
+})
 
 
+//grab city value which is a string input and then using geocode
+//.then same as .done
 
+// function convertDateTime(dtNum){
+//
+//     var dtInMs = dtNum * 1000 //seconds * 1000= our milliseconds
+//
+//     Var dateObj = new Date(dtInMs); // pass in milliseconds to date(constructor)
+//
+//     Return dateObj.toLocalString();
+//
+// }
 
-
-
-
+//.toLocalString returns string with date plus time
 
